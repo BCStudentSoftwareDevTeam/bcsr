bde44a6978aab688aea6dd154812f44cb46166ab
Cody Myers <codymyers63@gmail.com>
2016-04-21 21:58:45 +0000

    WORKING ON: (archive.py, archive.html) Still building the archive system

--- app/archive.py
+++ app/archive.py
@@ -1,8 +1,15 @@
 from allImports import *
 
-@app.route("/archive", methods = ["GET"])
-
-def archive():
+@app.route("/archive/", defaults={'post_SEID': None}, methods = ["GET", "POST"])
+@app.route("/archive/<post_SEID>", methods = ["GET", "POST"])
+def archive(post_SEID):
+    #SET DEFAULTS
+    semesters = Semesters.select()
+    if post_SEID != None:
+        #PUT RETRIEVAL INFORMATION IN HERE
+        comment = 'above'
     return render_template("archive.html",
-                            cfg = cfg
+                            cfg = cfg,
+                            semesters = semesters,
+                            SEID      = post_SEID
                             )
