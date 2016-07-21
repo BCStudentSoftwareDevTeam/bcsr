546628bb113920cc481d15cec1b74fcc272f5185
Cody Myers <codymyers63@gmail.com>
2016-07-21 14:58:53 +0000

    Creating the archive functionality

--- app/archive.py
+++ app/archive.py
@@ -5,17 +5,17 @@ from app.logic.getAll import GetAll
 @app.route("/archive/", defaults={'SEID': None}, methods = ["GET", "POST"])
 @app.route("/archive/<SEID>", methods = ["GET", "POST"])
 def archive(SEID):
+    getAll = GetAll()
     semesters = databaseInterface.grab_all_semesters()
     if SEID == None:
         SEID = databaseInterface.grab_current_semester()
+    two_dictionaries      = getAll.create_dictionaries(SEID)
     
-    
-    
-    
-        
     return render_template("archive.html",
-                        cfg = cfg,
+                        cfg       = cfg,
                         semesters = semesters,
-                        SEID      = SEID
+                        SEID      = SEID,
+                        divisions_to_programs = two_dictionaries[0],
+                        programs_to_courses   = two_dictionaries[1]
                         )
   
