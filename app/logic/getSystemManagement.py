72ba76bebfddb635d58effea08bd807e982f3a03
Cody Myers <codymyers63@gmail.com>
2016-08-02 17:23:25 +0000

    Created the functionality to add semesters for admin access

--- app/logic/getSystemManagement.py
+++ app/logic/getSystemManagement.py
@@ -1,5 +1,5 @@
 from app.allImports import *
-from app.logic import databaseInterface  
+from app.logic import databaseInterface 
 import time
 
 class GetSystemManagement():
@@ -24,49 +24,53 @@ class GetSystemManagement():
       years.append(str(year))
     return years
     
-    def create_seid(self, data):
-      '''Purpose: To create the term code based off of the year, and term
-      submited by form. Author: CDM 20160802
-      @Param -data {immutable dict} key_values = [year,term]
-      @key   -year => {string}
-      @key   -term => {integer}
-      '''
-      year = data['year']
-      term_key  = str(data['term'])
-      #same year is a list of all term keys that go off the current year, 
-      #instead of the previous year
-      same_year = [11]
-      for x in same_year:
-        if int(term_key) == x:
-          term_code == year + term_key
-        else:
-          term_code == str(int(year)-1) + term_key
-      return term_code
-    
-    def add_semester(self, data):
-      '''Purpose: To add the semester to the database, with the correct SEID, 
-      and term_name given data. Author: CDM 20160802
-      @Param -data {immutable dict} key_values = [year,term]
-      @key   -year => {string}
-      @key   -term => {integer}
-      '''
-      SEID          = self.create_seid(data)
-      checkSeid     = Semesters.select().where(Semester.SEID == SEID)
-      term_name = cfg['termInfo'][data['term']]
-      if checkSeid:
-        #TODO: Log error
-        message = "Semester: {0} {1} already exsists in the database.".format(term_name, data['year'])
-        return message
+  def create_seid(self, data):
+    '''Purpose: To create the term code based off of the year, and term
+    submited by form. Author: CDM 20160802
+    @Param -data {immutable dict} key_values = [year,term]
+    @key   -year => {string}
+    @key   -term => {integer}
+    '''
+    print 'inside create'
+    year = data['year']
+    term_key  = str(data['term'])
+    #same year is a list of all term keys that go off the current year, 
+    #instead of the previous year
+    same_year = [11]
+    for x in same_year:
+      if int(term_key) == x:
+        seid = year + term_key
       else:
-        try:
-          newSemester = Semester(SEID = int(SEID),
-                                 term = term_name,
-                                 year = int(data['year']))
-          newSemester.save(force_insert=True)
-          return True
-        except Exception as e:
-          #TODO: Log error
-          message = "An error occurred while trying to create semester {0} {1}".format(termname, data['year'])
-          return message
+        seid = str(int(year)-1) + term_key
+    
+    print "This is the SEID: " + str(seid)
+    return seid
+  
+  def add_semester(self, data):
+    '''Purpose: To add the semester to the database, with the correct SEID, 
+    and term_name given data. Author: CDM 20160802
+    @Param -data {immutable dict} key_values = [year,term]
+    @key   -year => {string}
+    @key   -term => {integer}
+    '''
+    SEID          = self.create_seid(data)
+    checkSeid     = Semesters.select().where(Semesters.SEID == SEID)
+    term_name     = cfg['termInfo'][int(data['term'])]
+    logType       = "ERROR"
+    if checkSeid:
+      #TODO: Log error
+      message = "Semester: {0} {1} already exsists in the database.".format(term_name, data['year'])
+    else:
+      try:
+        newSemester = Semesters(SEID = int(SEID),
+                               term = term_name,
+                               year = int(data['year']))
+        newSemester.save(force_insert=True)
+        message = "Semester: {0} {1} has been created successfully.".format(term_name, data['year'])
+        logType = "INFO"
+      except Exception as e:
+        #TODO: Log error
+        message = "An error occurred while trying to create semester {0} {1}".format(termname, data['year'])
+    return [logType,message]
                                   
   
--- app/systemManagement.py
+++ app/systemManagement.py
@@ -1,9 +1,11 @@
 from allImports import *
 from app.logic.getAuthUser import AuthorizedUser
+from app.logic.redirectBack import redirect_url
 from app.logic.getSystemManagement import GetSystemManagement
 
 @app.route("/admin/systemManagement", methods=["GET", "POST"])
 def systemManagement():
+  page = "/" + request.url.split("/")[-1]
   authorizedUser = AuthorizedUser()
   if authorizedUser.isAdmin:
     system    = GetSystemManagement()
@@ -17,18 +19,18 @@ def systemManagement():
                             years     = years,
                             )
                             
-@app.route("/admin/systemMangement/add", methods=["POST"])
+@app.route("/admin/systemManagement/add", methods=["POST","GET"])
 def addSemester():
+  page = "/" + request.url.split("/")[-1]
   authorizedUser = AuthorizedUser()
-  if authorizedUser.isAmin:
+  if authorizedUser.isAdmin:
     system      = GetSystemManagement()
     data        = request.form
-    addSemester = system.add_semester(data)
-    if addSemeser != True:
-      #Flash error message
-      flash(addSemester)
-    else:
-      flash("Semester successfully created.")
-    redirect(redirect_url())
+    logList         = system.add_semester(data)
+    print logList
+    #TODO: figure out how to log
+    #log.writer(logList[0],page,logList[1])
+    flash(logList[1])
+    return redirect(redirect_url())
       
                             
