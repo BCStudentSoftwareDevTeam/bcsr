968907b1d185a43b463de63502f27eb99e6e2520
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-08-17 19:29:22 +0000

    add collapsible

--- app/static/js/local/courses.js
+++ app/static/js/local/courses.js
@@ -37,3 +37,18 @@ function showPrograms(division) {
     $('#icon-'+ division).toggleClass('glyphicon-plus')
     $('#icon-'+ division).toggleClass('glyphicon-minus')
 }
+
+
+function showCourses(program) {
+    var programID = '#courses-'+ program;
+    if($(programID).css('display') == 'none'){
+        $(programID).css('display', 'block');
+        
+        
+    } else {
+        $(programID).css('display', 'none');
+    }
+    
+    $('#coursesIcon-'+ programs).toggleClass('glyphicon-plus')
+    $('#coursesIcon-'+ programs).toggleClass('glyphicon-minus')
+}
