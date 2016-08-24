9a1f88c0d436ac3852da88ecd3b4f895461aaf0a
Scott Heggen <sheggen@gmail.com>
2016-08-24 17:47:59 +0000

    SYNTAX

--- app/logic/getAuthUser.py
+++ app/logic/getAuthUser.py
@@ -14,7 +14,7 @@ class AuthorizedUser:
   def get_user(self):
     '''retruns the user object corresponding to the logged on user'''
     user = Users.select().where(Users.username == self.username)
-    if user is not None:
+    if user.exists():
       return user
     else:
       return 0
