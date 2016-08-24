07fbd03b7ec056f7d5aea150c174e3e65d3c7d24
Scott Heggen <sheggen@gmail.com>
2016-08-24 17:19:42 +0000

    Modified language, and allowing zip files

--- app/static/js/local/courses.js
+++ app/static/js/local/courses.js
@@ -4,7 +4,8 @@ Dropzone.options.drop = {
     paramName: "file", // The name that will be used to transfer the file
     maxFilesize: 5, // MB
     maxFiles: 1,
-    acceptedFiles: ".doc,.docx,.pdf,.txt",
+    acceptedFiles: ".doc,.docx,.pdf,.txt, .zip",
+    dictDefaultMessage: "Upload your syllabus here",
     uploadMultiple: false,
     addRemoveLinks: true,
     clickable: true,
