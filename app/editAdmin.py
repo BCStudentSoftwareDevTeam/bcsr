4fcf6ec0547c40610d14deec864cf98a028551fa
Cody Myers <codymyers63@gmail.com>
2017-01-25 18:41:42 +0000

    Added a controller for managing Admins

---
+++ app/editAdmin.py
@@ -0,0 +1,19 @@
+from allImports import *
+from app.logic.getAuthUser import AuthorizedUser
+from app.logic.redirectBack import redirect_url
+
+@app.route('/editAdmin/<action>', methods=["POST"])
+def editAdmin(action):
+  authorizedUser = AuthorizedUser()
+  if authorizedUser.isAdmin:
+    if action == 'add':
+      message = "User: ( " + username + " ) has been added as an Admin."
+    elif action == 'remove':
+      message = "User: ( " + username + " ) has been removed as an Admin."
+    else:
+      message = 'An error occured while trying to edit data.' 
+      #TODO: LOG HERE
+    flash(message)
+    return redirect(redirect_url('systemManagement'))
+    else:
+      abort(403)
