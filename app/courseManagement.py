d8f1ba5a602f168b95bf5e31838217ec22351f0e
Cody Myers <codymyers63@gmail.com>
2017-02-07 22:34:10 +0000

    feat: Added pages for sidebar & generating missing syllabi
    I created the sidebar for courseManagement, the options for the sidebar are saved in config.yaml. I also created some pages for searching for missing syllabi which is a sidebar option.

--- app/courseManagement.py
+++ app/courseManagement.py
@@ -7,5 +7,19 @@ def courseManagement():
     authorizedUser = AuthorizedUser()
     return render_template('admin/courseManagement.html',
                             cfg = cfg,
-                            isAdmin       = authorizedUser.isAdmin,
+                            isAdmin       = authorizedUser.isAdmin
                             )
+
+@app.route("/admin/courseManagement/missingSyllabi", methods=["GET"])
+def missingSyllabi():
+    authorizedUser = AuthorizedUser()
+    activePage = "snips/missingSyllabi.html"
+    # activePage should be the name of the snip that should contain the content
+    # of the page-wrapper section of courseMangement.html
+    semesters = Semesters.select()
+    return render_template('admin/courseManagement.html',
+                            cfg           = cfg,
+                            isAdmin       = authorizedUser.isAdmin,
+                            activePage    = activePage,
+                            semesters     = semesters)
+    
