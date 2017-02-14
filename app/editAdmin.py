3c646d7ac93fd362c46cb40c4ca06837f932b8f0
Cody Myers <codymyers63@gmail.com>
2017-02-14 22:10:17 +0000

    feat: working on removing and adding Admins

--- app/editAdmin.py
+++ app/editAdmin.py
@@ -2,20 +2,35 @@ from allImports import *
 from app.logic.getAuthUser import AuthorizedUser
 from app.logic.redirectBack import redirect_url
 
-@app.route('/editAdmin/<action>', methods=["POST"])
-def editAdmin(action):
-  authorizedUser = AuthorizedUser()
-  if authorizedUser.isAdmin:
-    
-    if action == 'add':
-      #message = "User: ( " + username + " ) has been added as an Admin."
-      message = "User: ( Test Message ) has been added as an Admin."
-    elif action == 'remove':
-      message = "User: ( Test Message ) has been removed as an Admin."
+@app.route('/editAdmin', methods=["POST"])
+def editAdmin():
+  try:
+    username = authUser(request.environ)
+    authorizedUser = AuthorizedUser()
+    if authorizedUser.isAdmin:
+      data = request.form
+      if data['admin[]'] != "" and data['admin'] is not None: 
+        user = Users.get(Users.username == data['admin[]'])
+        print user.isAdmin
+        if user.isAdmin:
+          message = "User: ({}) has been removed as an Admin.".format(user.username)
+        else:
+          message = "User: ({}) has been added as an Admin.".format(user.username)
+        #flip the boolean
+        user.isAdmin = not user.isAdmin
+        user.save()
+        #TODO: LOG HERE
+        flash(message)
+        return redirect(redirect_url('systemManagement'))
+      else:
+        flash("No user was selected.")
+        return redirect(redirect_url('systemManagement'))
     else:
-      message = 'An error occured while trying to edit data.' 
-      #TODO: LOG HERE
-    flash(message)
+      abort(403)
+  except Exception as e:
+    #TODO: Log Message
+    message = "An error occured while processing data"
+    flash(e)
     return redirect(redirect_url('systemManagement'))
-  else:
-    abort(403)
+
+  
