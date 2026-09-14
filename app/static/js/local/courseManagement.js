14cfb7cff99cd3c701a91a1a63b4d17e8d682f76
bakobagassas <bakobagassas@berea.edu>
2026-09-14 19:35:47 -0400

    added validity tag so that the user gets actual feedback when the input is not valid

---
+++ app/static/js/local/courseManagement.js
@@ -0,0 +1,5 @@
+$("#number").on("input", function(){
+    if ($(this).val() && isNaN($(this).val())) {
+        this.setCustomValidity("Please enter numbers only.");
+        this.reportValidity();
+    } else {this.setCustomValidity("");}});
