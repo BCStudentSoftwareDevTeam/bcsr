e45b6ffa261551040412fb563fbef0db8fb46843
conwelld <conwelld@berea.edu>
2026-09-15 10:45:42 -0400

    Revert "made changes to app.js and messages.html to have a fade out to pop up messages when adding course"
    This reverts commit 2009005192247a182f0bc0c411ce30020da1daa2.

--- app/static/js/local/app.js
+++ app/static/js/local/app.js
@@ -2,10 +2,4 @@ $(function() {
 
     $('#side-menu').metisMenu({ toggle: false });
 
-    window.setTimeout(function() {
-    $('.flasher').fadeOut(600, function() {
-        $(this).alert('close');
-    });
-}, 5000);
-
 });
