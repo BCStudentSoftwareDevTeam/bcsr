2009005192247a182f0bc0c411ce30020da1daa2
conwelld <conwelld@berea.edu>
2026-09-15 10:37:07 -0400

    made changes to app.js and messages.html to have a fade out to pop up messages when adding course

--- app/static/js/local/app.js
+++ app/static/js/local/app.js
@@ -2,4 +2,10 @@ $(function() {
 
     $('#side-menu').metisMenu({ toggle: false });
 
+    window.setTimeout(function() {
+    $('.flasher').fadeOut(600, function() {
+        $(this).alert('close');
+    });
+}, 5000);
+
 });
