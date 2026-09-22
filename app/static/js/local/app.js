4057cd8df9ba79e91b54af38dfa7b1ac276c397a
Alpha Nyabuto <Nyabutoa@berea.edu>
2026-09-22 10:53:31 -0400

    updated the function to work past the jquery blockers

--- app/static/js/local/app.js
+++ app/static/js/local/app.js
@@ -3,7 +3,12 @@ $(function() {
     $('#side-menu').metisMenu({ toggle: false });
 
     window.setTimeout(function() {
-        $('.alert.alert-dismissible').alert('close');
+        document.querySelectorAll('.alert.alert-dismissible').forEach(function(alert) {
+            alert.remove();
+        });
     }, 5000);
 
 });
+
+// when one makes a deadline this is supposed to make the popup close automatically after 5 seconds.
+// https://www.w3schools.com/jsref/met_win_settimeout.asp
